5689a47466a7d0210c572a3a24a1b6251ba696a9
pooja <poojaeb123@gmail.com>
2025-02-14 16:37:22 +0530

    project initiated

---
+++ src/test/java/com/example/java_git_demo/JavaGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
