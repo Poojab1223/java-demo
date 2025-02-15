6447d684f50debde468becb02c022eac6e982362
pooja <poojaeb123@gmail.com>
2025-02-15 16:07:57 +0530

    adding user entity

---
+++ src/main/java/com/example/java_git_demo/User.java
@@ -0,0 +1,23 @@
+package com.example.java_git_demo;
+
+public class User {
+    private int id;
+    private String name;
+    //private String email;
+
+    public void setId(int String){
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
